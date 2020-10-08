9e5b8425333d045883095e9f21744e745cc7ca21
Arto Bendiken <arto@bendiken.net>
2020-10-09 00:28:34 +0300

    Outlined the library structure.

--- lib/near.dart
+++ lib/near.dart
@@ -1,6 +1,15 @@
 /* This is free and unencumbered software released into the public domain. */
 
-/// NEAR client for Flutter.
+/// NEAR protocol client for Flutter.
 ///
 /// To use, import `package:near/near.dart`.
 library near;
+
+export 'src/account.dart';
+export 'src/connection.dart';
+export 'src/errors.dart';
+export 'src/key_pair.dart';
+export 'src/serialize.dart';
+export 'src/signer.dart';
+export 'src/transaction.dart';
+export 'src/validator.dart';
---
+++ test/key_pair_test.dart
@@ -0,0 +1,9 @@
+/* This is free and unencumbered software released into the public domain. */
+
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:near/near.dart'; // ignore_for_file: unused_import
+
+void main() {
+  // TODO
+}
