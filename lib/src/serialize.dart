5888990e50e9a647194f838a1f96f6269f34768e
Arto Bendiken <arto@bendiken.net>
2020-10-10 15:48:11 +0300

    Implemented BinaryReader#readU*().

--- lib/src/serialize.dart
+++ lib/src/serialize.dart
@@ -1,6 +1,6 @@
 /* This is free and unencumbered software released into the public domain. */
 
-import 'dart:typed_data' show ByteData, Uint8List;
+import 'dart:typed_data' show ByteData, Endian, Uint8List;
 
 class BinaryWriter {
   const BinaryWriter();
@@ -21,22 +21,33 @@ class BinaryReader {
   }
 
   int readU16() {
-    return 0; // TODO
+    final result = _input.getUint16(_offset, Endian.little);
+    _offset += 2;
+    return result;
   }
 
   int readU32() {
-    return 0; // TODO
+    final result = _input.getUint32(_offset, Endian.little);
+    _offset += 4;
+    return result;
   }
 
   int readU64() {
-    return 0; // TODO
+    final result = _input.getUint64(_offset, Endian.little);
+    _offset += 8;
+    return result;
   }
 
-  int readU128() {
-    return 0; // TODO
+  BigInt readU128() {
+    BigInt result = BigInt.zero;
+    for (int i = 15; i >= 0; i--) {
+      result += BigInt.from(this.readU8()) << (8 * i);
+    }
+    return result;
   }
 
   String readString() {
+    //final len = this.readU32();
     return null; // TODO
   }
 
@@ -45,6 +56,7 @@ class BinaryReader {
   }
 
   List<dynamic> readArray() {
+    //final len = this.readU32();
     return null; // TODO
   }
 }
